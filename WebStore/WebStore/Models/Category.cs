da6894846419dc1ef11fdf0414860ceed65a7af4
Erich <erichfmello@gmail.com>
2021-02-25 11:30:48 -0300

    Create Category.cs

---
+++ WebStore/WebStore/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int _idCategory { get; set; }
+        public string Name { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(int idCategory, string name)
+        {
+            _idCategory = idCategory;
+            Name = name;
+        }
+    }
+}
