3802754bba0a052bbde8a47cbfac7246997e845d
Erich <erichfmello@gmail.com>
2021-02-25 11:42:21 -0300

    Create ShoppingCart.cs

---
+++ WebStore/WebStore/Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public string _cpf { get; private set; }
+        public int _idProduct { get; private set; }
+        public int Amount { get; set; }
+
+        public People People { get; set; }
+        public Product Product { get; set; }
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string cpf, int idProduct, int amount)
+        {
+            _cpf = cpf;
+            _idProduct = idProduct;
+            Amount = amount;
+        }
+    }
+}
